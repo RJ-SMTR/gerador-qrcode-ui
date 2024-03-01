ff571c84b0392892c74e48e54c8d4f84cf38ce37
Raphael Rivas <raphaelrivas@hotmail.com>
2024-03-01 10:26:31 -0300

    feat: generate a list of codes

--- generator.py
+++ generator.py
@@ -14,11 +14,6 @@ OUTPUT_DIR: Path = Path("output")
 URL_TEMPLATE: jinja2.Template = jinja2.Template(
     "https://mobilidade.rio/{{ code }}")
 
-
-def generate_code(charset: Union[str, List[str]] = CHARSET, length: int = LENGTH) -> str:
-    return "".join(random.choice(charset) for _ in range(length))
-
-
 def generate_url(code: str) -> str:
     return URL_TEMPLATE.render(code=code)
 
@@ -27,24 +22,18 @@ def generate_qr(code: str) -> str:
     img = qrcode.make(generate_url(code))
     img.save(OUTPUT_DIR / f"{code}.png")
 
-
 if __name__ == "__main__":
     if len(argv) != 2:
-        print("Usage: python3 generator.py <number of codes>")
+        print("Usage: python3 generator.py <list of codes separated by commas>")
         exit(1)
 
-    try:
-        number_of_codes = int(argv[1])
-    except ValueError:
-        print("Invalid number of codes")
-        exit(1)
+    codes_list = argv[1].split(',')
 
     if not OUTPUT_DIR.exists():
         OUTPUT_DIR.mkdir()
 
-    for _ in tqdm(range(number_of_codes)):
-        code: str = generate_code()
+    for code in tqdm(codes_list):
         generate_qr(code)
 
-    print(f"Generated {number_of_codes} codes")
+    print(f"Generated {len(codes_list)} codes")
     print(f"Output directory: {OUTPUT_DIR}")
